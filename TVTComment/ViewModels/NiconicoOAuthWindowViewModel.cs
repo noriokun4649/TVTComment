77e3fad0976ca42aa2b3c2aa4d3280fe60a132b1
noriokun4649 <noriokun.official.mail@gmail.com>
2021-05-18 14:17:57 +0900

    OAuth認可ウィンドウついか

---
+++ TVTComment/ViewModels/NiconicoOAuthWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace TVTComment.ViewModels
+{
+    class NiconicoOAuthWindowViewModel : BindableBase
+    {
+        public NiconicoOAuthWindowViewModel()
+        {
+
+        }
+    }
+}
---
+++ TVTComment/Views/NiconicoOAuthWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TVTComment.Views
+{
+    /// <summary>
+    /// NiconicoOAuth.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NiconicoOAuthWindow : Window
+    {
+        public NiconicoOAuthWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
