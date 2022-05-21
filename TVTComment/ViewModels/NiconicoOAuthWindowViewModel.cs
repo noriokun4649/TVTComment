fc5d664c38e53683c237eda99fbe470e6db2c850
noriokun4649 <noriokun.official.mail@gmail.com>
2022-05-21 09:01:37 +0900

    Add webview2 viewmodel.

--- TVTComment/ViewModels/NiconicoOAuthWindowViewModel.cs
+++ TVTComment/ViewModels/NiconicoOAuthWindowViewModel.cs
@@ -1,11 +1,25 @@
-﻿using Prism.Mvvm;
+﻿using Microsoft.Web.WebView2.Core;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Windows;
 
 namespace TVTComment.ViewModels
 {
     class NiconicoOAuthWindowViewModel : BindableBase
     {
-        public NiconicoOAuthWindowViewModel()
+        public ReactiveCommand<CoreWebView2HttpResponseHeaders> RequestRecived { get; } = new ReactiveCommand<CoreWebView2HttpResponseHeaders>();
+
+        public NiconicoOAuthWindowViewModel(Model.TVTComment model)
         {
+            RequestRecived.WithSubscribe(resp => OnRequestRecived(resp));
+        }
+
+        private void OnRequestRecived(CoreWebView2HttpResponseHeaders resp)
+        {
+            var token = resp.GetHeader("TvTCmt-Server-AccessToken");
+            var scorp = resp.GetHeader("TvTCmt-Server-Scope");
+
+            MessageBox.Show(token+scorp);
 
         }
     }
