bab14b992ffeb973c854ab6bbdb3b88b8d77b50c
noriokun4649 <noriokun.official.mail@gmail.com>
2022-08-28 16:33:54 +0900

    Add Panel resource. #60

---
+++ Viewer/PanelResource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// Panel.rc で使用
+//
+#define IDD_TVTCOM_PANEL                101
+#define IDD_LOG_TAB                     103
+#define IDD_SELECT_TAB                  104
+#define IDC_TAB1                        1001
+#define IDC_COMBOBOXEX1                 1002
+#define IDC_COMMENT_SEND                1003
+#define IDC_FROM_LIST                   1004
+#define IDC_CURRENT_LIST                1005
+#define IDC_ADD_BUTTON                  1006
+#define IDC_DELETE_BUTTON               1007
+#define IDC_AUTO_SELECT_CHECK           1008
+#define IDC_LOG_LIST                    1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
